fa6be26dc9788cebf70a2706d12b816a2277b0fb
Felix <zen9.felix@gmail.com>
2022-02-21 12:21:46 +0800

    Add stack logic

---
+++ src/coding_quizzes/queue_and_stack.clj
@@ -0,0 +1,30 @@
+(ns coding-quizzes.queue-and-stack)
+
+;; ==============================
+;; use vector as queue
+
+(defn enqueue [q & items]
+  (apply conj q items))
+
+(defn dequeue [q & [n]]
+  (list (take (or n 1) q)
+        (drop (or n 1) q)))
+
+(comment
+  (enqueue [1 2 3] 4 5)
+  (dequeue [1 2 3 4 5])
+  ;; or simply destructure
+  (let [[first & rest] [1 2 3 4 5]]
+    [first rest]))
+
+;; ==============================
+;; Stack
+;; again, use vector
+
+(comment
+  ;; push to stack
+  (conj [1 2 3] 4 5)
+  ;; pop stack
+  (pop [1 2 3])
+  ;; peek stack
+  (peek [1 2 3]))
