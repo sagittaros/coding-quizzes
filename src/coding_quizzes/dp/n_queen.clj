acd549993d6392498e9fb8ec282a19af6e4d0b9d
Felix <zen9.felix@gmail.com>
2022-04-09 12:20:59 +0800

    Improved code with run!

--- src/coding_quizzes/dp/n_queen.clj
+++ src/coding_quizzes/dp/n_queen.clj
@@ -26,28 +26,27 @@
          :diag1 (vec (repeat (inc (* 2 (dec n))) false))
          :diag2 (vec (repeat (inc (* 2 (dec n))) false))})
       (search [y {:keys [board cols diag1 diag2] :as state}]
-        (let [mode (atom :contd)]
-          (doseq [x (range n)]
-            (when (= @mode :contd)
-              (let [d1-idx (+ x y)
-                    d2-idx (-> x (- y) (+ n) dec)]
-                (cond
-                  (= y n) ;; terminate
-                  (do
-                    (swap! count inc)
-                    (swap! valid-solutions conj board)
-                    (swap! mode :terminate))
+        (run! ;; run! is similar to reduce
+         (fn [x] (let [d1-idx (+ x y)
+                       d2-idx (-> x (- y) (+ n) dec)]
+                   (cond
+                     (= y n) ;; beyond bound, solved!
+                     (do
+                       (swap! count inc)
+                       (swap! valid-solutions conj board)
+                       (reduced :done)) ;; reduced prematuraly terminate iteration, like `break`
 
-                  (or (cols x) (diag1 d1-idx) (diag2 d2-idx)) ;; clashed
-                  :clashed
+                     (or (cols x) (diag1 d1-idx) (diag2 d2-idx)) ;; clashed
+                     :clashed
 
-                  :else ;; continue search by moving down the row
-                  (search (inc y)
-                          (-> state
-                              (update :board #(assoc-in % [x y] 1))
-                              (update :cols #(assoc % x true))
-                              (update :diag1 #(assoc % d1-idx true))
-                              (update :diag2 #(assoc % d2-idx true))))))))))]
+                     :else ;; continue search by moving down the row
+                     (search (inc y)
+                             (-> state
+                                 (update :board #(assoc-in % [x y] 1))
+                                 (update :cols #(assoc % x true))
+                                 (update :diag1 #(assoc % d1-idx true))
+                                 (update :diag2 #(assoc % d2-idx true)))))))
+         (range n)))]
       (search 0 (mk-state))
       [@count @valid-solutions])))
 
@@ -56,7 +55,7 @@
   ;; (diag-rtl-debug 6)
 
   (println "------")
-  (n-queen 4)
+  (first (n-queen 5))
 
 ;; column
   [[0 1 2 3]
