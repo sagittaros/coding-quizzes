15c56cb6fb655d843a174a24502383e0e0e96fa5
Felix <zen9.felix@gmail.com>
2022-04-11 13:47:39 +0800

    Matrix addition

--- src/coding_quizzes/matrix/matrix_addition.clj
+++ src/coding_quizzes/matrix/matrix_addition.clj
@@ -2,33 +2,23 @@
   (:require
    [coding-quizzes.matrix.matrix-generation :refer [gen-ndim-matrix]]))
 
- ;; (defn- gen-unimatrix [dims]
+(defn traverse [outer & [f idxs]]
+  (if-not (vector? outer)
+    ((or f list) (->> idxs reverse vec) outer)
+    (mapv (fn [[i inner]]
+            (traverse inner f (conj idxs i)))
+          (zipmap (range) outer))))
 
- ;;  (mapv (fn [i]
- ;;          (mapv (fn [j] [i j])
- ;;                (range J))) (range I)))
-
-(def ^:private m1
-  [[0 1]
-   [2 0]])
-
-(def ^:private m2
-  [[1 3]
-   [4 1]])
+(defn add-matrix [M1 M2]
+  (let [add-with (fn [A idxs x] (+ x (get-in A idxs)))]
+    (traverse M1 (partial add-with M2))))
 
 (comment
+  (let [M1 (gen-ndim-matrix 4 (partial apply +))
+        M2 (gen-ndim-matrix 4 (partial apply *))]
+    (add-matrix M1 M2))
+
   (println "===")
+  (traverse (gen-ndim-matrix 3 (partial apply +)))
   (map vector (range 3) (range 3))
   (map (partial apply +) [[1 2] [3 4] [4 6]]))
-
-(comment
-  (gen-ndim-matrix 3)
-  [[[[0 0 0] [1 0 0] [2 0 0]]
-    [[0 1 0] [1 1 0] [2 1 0]]
-    [[0 2 0] [1 2 0] [2 2 0]]]
-   [[[0 0 1] [1 0 1] [2 0 1]]
-    [[0 1 1] [1 1 1] [2 1 1]]
-    [[0 2 1] [1 2 1] [2 2 1]]]
-   [[[0 0 2] [1 0 2] [2 0 2]]
-    [[0 1 2] [1 1 2] [2 1 2]]
-    [[0 2 2] [1 2 2] [2 2 2]]]])
