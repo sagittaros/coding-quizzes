f75f18e004aab5496668c4b68366e58f3f085834
Felix <zen9.felix@gmail.com>
2022-04-11 14:42:24 +0800

    Hide bad code

--- src/coding_quizzes/matrix/matrix_generation.clj
+++ src/coding_quizzes/matrix/matrix_generation.clj
@@ -7,20 +7,20 @@
     ((or f #(-> % reverse vec)) idxs)
     (mapv #(gen-ndim-matrix n f (conj idxs %)) (range n))))
 
-(defn- gen3d
-  "naive method"
-  []
-  (mapv (fn [i]
-          (mapv (fn [j]
-                  (mapv (fn [k]
-                          [i j k])
-                        (range 3))) (range 3))) (range 3)))
+;; (defn- gen3d
+;;   "naive method"
+;;   []
+;;   (mapv (fn [i]
+;;           (mapv (fn [j]
+;;                   (mapv (fn [k]
+;;                           [i j k])
+;;                         (range 3))) (range 3))) (range 3)))
 
 (comment
   (println "===")
   (gen-ndim-matrix 2)
   (gen-ndim-matrix 3 (partial str/join ":"))
   (gen-ndim-matrix 3 (partial apply +))
-  (gen3d)
+  ;; (gen3d)
   (map vector (range 3) (range 3))
   (map (partial apply +) [[1 2] [3 4] [4 6]]))
