60ef129ce2d5d98355f20b2264f4040a6d3df601
Felix <zen9.felix@gmail.com>
2022-05-27 21:19:02 +0800

    Delete core_test.clj

--- test/coding_quizzes/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns coding-quizzes.core-test
-  (:require [clojure.test :refer :all]
-            [coding-quizzes.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
